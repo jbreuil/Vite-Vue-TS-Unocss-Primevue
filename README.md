025f1dd05666d04c5dad86f78c05ccb24045ddfd
Julien <jbreuil@gmail.com>
2023-09-05 20:40:00 +0200

    change config and readme

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
+# Unocss / Primevue
+
+Primevue services needs to be imported in main.ts by default only primevue components are auto imported.
+
+To use icons from unocss, install corresponding iconify collection.
+
 # Vue 3 + TypeScript + Vite
 
 This template should help get you started developing with Vue 3 and TypeScript in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
 import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
 import { PrimeVueResolver } from 'unplugin-vue-components/resolvers'
-import VueRouter from 'unplugin-vue-router/vite'
+import Components from 'unplugin-vue-components/vite'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
+import VueRouter from 'unplugin-vue-router/vite'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,7 +17,7 @@ export default defineConfig({
     AutoImport({
       dts: true,
       dirs: [
-        './composables', // only root modules
+        './src/composables', // only root modules
       ],
       vueTemplate: true,
       imports: [
